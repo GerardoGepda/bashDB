afe64741586c45c4277282767d217c967298c99a
GerardoGepda <gerardo.gepda@gmail.com>
2022-11-13 18:22:01 +0000

    solución de error

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 fundialog = ${fundialog=dialog}
-opcion =`$fundialog --stdout --menu "bashDB - Menú Principal" 0 0 0 1 "Ingresar Libro" 2 "Ingresar Persona" 3 "Ingresar Préstamo" 4 "Consultar Libro" 5 "Consultar Persona" 6 "Consultar Préstamo" 7 "Actualizar Libro" 8 "Actualizar Persona" 9 "Mostrar Integrantes de grupo" 10 "Salir"`
+opcion=`$fundialog --stdout --menu "bashDB - Menú Principal" 0 0 0 1 "Ingresar Libro" 2 "Ingresar Persona" 3 "Ingresar Préstamo" 4 "Consultar Libro" 5 "Consultar Persona" 6 "Consultar Préstamo" 7 "Actualizar Libro" 8 "Actualizar Persona" 9 "Mostrar Integrantes de grupo" 10 "Salir"`
+clear
 echo $opcion
