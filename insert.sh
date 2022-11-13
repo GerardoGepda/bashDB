29d43322dac25e07178867f029e746580b3a6f40
GerardoGepda <gerardo.gepda@gmail.com>
2022-11-13 22:18:01 +0000

    se termino el insert

--- insert.sh
+++ insert.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-file="$1"
+file="tables/$1"
 cont=0
+newID=0
 while read -r line; do
-   echo -e "$line\n"
    let cont++
 done <$file
 
-echo "contador: $cont"
+#creando nuevo id
+let newID=cont+1
 
 case $1 in
    "libro")
-      echo "elegiste libro"
       if [[ ! -z $2 ]] && [[ ! -z $3 ]] && [[ ! -z $4 ]]
       then
-         echo "1;$2;$3;$4"
+         echo "$newID;$2;$3;$4" >> $file
+         dialog --title "INSERT" --msgbox "Se inserto el libro" 0 0
       else
          echo "Datos incompletos"
       fi
    ;;
    "persona")
-      echo "elegiste persona"
       if [[ ! -z $2 ]]
       then
-         echo "todos los parametros listos"
+         echo "$newID;$2" >> $file
+         dialog --title "INSERT" --msgbox "Se inserto la persona" 0 0
       else
          echo "Datos incompletos"
       fi
    ;;
    "prestamo")
-      echo "elegiste prestamo"
       if [[ ! -z $2 ]] && [[ ! -z $3 ]] && [[ ! -z $4 ]]
       then
-         echo "todos los parametros listos"
+         echo "$newID;$2;$3;$4" >> $file
+         dialog --title "INSERT" --msgbox "Se inserto el prestamo" 0 0
       else
          echo "Datos incompletos"
       fi
