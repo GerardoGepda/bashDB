82313f79210eca4b837a6f054f5133361c79e0ab
GerardoGepda <gerardo.gepda@gmail.com>
2022-11-14 14:21:11 -0600

    pequeño cambio en select

--- select.sh
+++ select.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -z $1 ]]; then
    file="tables/$1"
-   info=$(cat $file | awk -F ";" '{print $1"\t"$2"\t"$3"\t"$4}')
+   info=$(awk -F ";" '{print $1"\t"$2"\t"$3"\t"$4}' $file)
    dialog --title "Tabla: $1" --msgbox "$info" 0 0
 else
    dialog --title "Error" --msgbox "Debe especificar la tabla" 0 0
